3abf31f2eea09df633c80953e2b26ec8cc83f767
veiganlime <105752348+veiganlime@users.noreply.github.com>
2023-03-17 14:48:57 +0100

    prkt_4

---
+++ pyprakt_4.py
@@ -0,0 +1,12 @@
+print("Willkommen zum Test")
+
+print("Gib mir die Kantelaenge a")
+kante_laenge_a = float(input())
+print("Gib mir die Kantelaenge b")
+kante_laenge_b = float(input())
+
+umfang = kante_laenge_a + kante_laenge_b
+flaeche =kante_laenge_a * kante_laenge_b
+
+print("Kantenumfang des Rechteck ist ", umfang)
+print("Flaechenumfang des Rechteck ist ", flaeche)
