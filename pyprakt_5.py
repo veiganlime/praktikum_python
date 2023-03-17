759a3f2ef39bee2e819b46b70a2687c6581c1b5a
veiganlime <105752348+veiganlime@users.noreply.github.com>
2023-03-17 15:49:34 +0100

    prkt_5

---
+++ pyprakt_5.py
@@ -0,0 +1,13 @@
+x = 5
+if x == 1:
+    print("Erster Fall!")
+    print("Hier hat x den Wert 1")
+if x > 2:
+    print("Zweiter Fall!")
+    print("Hier hat x den Wert 2")
+if x == 5:
+    print("Hier hat x den Wert 5")
+if x == 7:
+    print("Resteverwertung!")
+    print("Hier werden alle restliche Faelle behandelt")
+print("Ende!")
