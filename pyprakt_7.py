473755aef498bf7b6fde7c88126da3271e5d73a1
veiganlime <105752348+veiganlime@users.noreply.github.com>
2023-03-17 21:19:22 +0100

    prkt_7

---
+++ pyprakt_7.py
@@ -0,0 +1,11 @@
+#Listen
+obst = ["Apfel","Birne","Banane"]
+verschiedenes = ["Eins",2,True,3.5,'Vier']
+geschachtelt = [obst,"-----",verschiedenes,3]
+tiere = [ ]
+zoo = list()
+
+
+
+
+print(obst[4:-4])
