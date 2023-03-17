f0e98857bcafe806dc7b1d844e67c7e03e5cf774
veiganlime <105752348+veiganlime@users.noreply.github.com>
2023-03-17 21:45:50 +0100

    prkt_8

--- pyprakt_8.py
+++ pyprakt_8.py
@@ -1,4 +1,4 @@
-#liste and Tupel
+#Liste and Tupel
 la = [1, 2, 3, 4]
 lb = la
 print("Liste vorher:", id(la), id(lb))
@@ -13,3 +13,35 @@ print("Tupel vorher:", id(ta), id(tb))
 ta += (5,)
 print(tb)
 print("Tupel nachher:", id(ta), id(tb))
+
+
+
+
+# Dictionary
+waehrung = {
+    "Deutschland" : "Euro",
+    "USA" : "US Dollar",
+    "England" : "Pfund",
+    "Japan" : "Yen"
+}
+kontostand = {
+    "Maier" : "123.45",
+    "Mueller" : "7854.54",
+    "Schulze" : "4567,89"
+}
+leeresDictionary = { }
+nochEinLeeresDictionary = dict()
+
+print("Dictionary ID vorher", id(waehrung))
+waehrung["Russland"] = "Rubel"
+print("Dictionary ID nachher", id(waehrung))
+print(waehrung["Russland"])
+print()
+
+
+
+print("Muellers Kontostand", kontostand["Mueller"])
+kontostand["Mueller"] = 0
+print("Muellers Kontostand nach der Aenderung", kontostand["Mueller"])
+kontostand["Mueller"] = -99
+print("Muellers Kontostand nach der 2. Aenderung", kontostand["Mueller"])
