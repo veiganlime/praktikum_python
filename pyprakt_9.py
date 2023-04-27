4033c902d302fd4a375b9a46c735404d87cc0867
veiganlime <105752348+veiganlime@users.noreply.github.com>
2023-04-27 12:58:01 +0200

    praktikum9

---
+++ pyprakt_9.py
@@ -0,0 +1,23 @@
+import sys
+
+def schaltjahr(eingabe):
+    if eingabe < 1583:
+        return None
+    return eingabe % 4 == 0 and eingabe % 100 != 0 or eingabe % 400 == 0
+
+print("Das ist ein Startmeldung!")
+
+try:
+    eingabe = int(input("Geben Sie ein Jahr ein!"))
+except ValueError:
+    print("Eingegebenen Wert ist kein Zahl!")
+    sys.exit(1)
+
+isSchaltjahr = schaltjahr(eingabe)
+
+if isSchaltjahr:
+    print(eingabe, "ist ein Schaltjahr")
+elif isSchaltjahr is None:
+    print("Jahr entspricht nicht dem Gregorianischen Kalender")
+else:
+    print(eingabe, "ist kein Schaltjahr")
